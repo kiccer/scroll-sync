884d03cffe8388bb9e2a877fb28703b1973c9c16
H171 <1091088708@qq.com>
2023-06-28 16:30:19 +0800

    refactor!: finish

---
+++ playground/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>default: X/Y轴同步、相对滚动</h1>
+    <SyncXYDefault />
+    <h1>X/Y轴同步、绝对滚动</h1>
+    <SyncXYAbsolute />
+    <h1>仅在 x 方向同步滚动</h1>
+    <SyncX />
+    <h1>仅在 y 方向同步滚动</h1>
+    <SyncY />
+  </div>
+</template>
